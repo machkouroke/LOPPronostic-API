2a9a1c41c346515b2cd36d38d1f52a096fb81633
machkouroke <machkour20.mo1@gmail.com>
2023-01-13 21:47:49 +0100

    `fix` Fix bug of environnement variable is not loaded

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv('.env'))
-X_Auth_Token = os.environ.get("X_Auth_Token")
+X_Auth_Token = os.environ.get("X_AUTH_TOKEN")
 GCLOUD_SERVICE_KEY_JSON = os.environ.get("GCLOUD_SERVICE_KEY_JSON").replace("'", '"')
 GCLOUD_CREDENTIAL_PATH = os.environ.get("GCLOUD_CREDENTIAL_PATH")
 GCLOUD_PROJECT_ID = os.environ.get("GCLOUD_PROJECT_ID")
