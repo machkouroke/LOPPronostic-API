5d3d6071c19d487e4c386a3bf6c898cbd1472240
machkouroke <machkour20.mo1@gmail.com>
2023-01-13 18:13:27 +0100

    `fix` Rename of GCLOUD_SERVICE_KEY to GCLOUD_SERVICE_KEY_JSON

--- config/__init__.py
+++ config/__init__.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv('.env'))
 X_Auth_Token = os.environ.get("X_Auth_Token")
-GCLOUD_SERVICE_KEY = os.environ.get("GCLOUD_SERVICE_KEY").replace("'", '"')
+GCLOUD_SERVICE_KEY_JSON = os.environ.get("GCLOUD_SERVICE_KEY_JSON").replace("'", '"')
 GCLOUD_CREDENTIAL_PATH = os.environ.get("GCLOUD_CREDENTIAL_PATH")
 GCLOUD_PROJECT_ID = os.environ.get("GCLOUD_PROJECT_ID")
 GCLOUD_REGION = os.environ.get("GCLOUD_REGION")
--- gcloud/__init__.py
+++ gcloud/__init__.py
@@ -1,8 +1,8 @@
 import os
-from config import GCLOUD_SERVICE_KEY, GCLOUD_CREDENTIAL_PATH
+from config import GCLOUD_SERVICE_KEY_JSON, GCLOUD_CREDENTIAL_PATH
 
 if not (os.path.exists(GCLOUD_CREDENTIAL_PATH)):
-    print(GCLOUD_SERVICE_KEY, file=open(GCLOUD_CREDENTIAL_PATH, mode="w"))
+    print(GCLOUD_SERVICE_KEY_JSON, file=open(GCLOUD_CREDENTIAL_PATH, mode="w"))
 
 from google.cloud import dataproc_v1
 
